c9474b58b4cc612d82dc40b13ffc03ade1b48bf6
loopnyapy <kakoyto_deniska@bk.ru>
2021-07-08 17:11:42 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry/sentry.dart';
+
+/// Custom [BlocObserver] which observes all bloc and cubit instances.
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) async {
+    print(error);
+    await Sentry.captureException(
+      error,
+      stackTrace: stackTrace,
+    );
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+Future<void> main() async {
+  Bloc.observer = SimpleBlocObserver();
+
+  await SentryFlutter.init(
+    (options) =>
+        options.dsn = 'https://6ce6b5d94a4c4d4ba47637329665c6f7@o913257.ingest.sentry.io/5850958',
+    appRunner: () => runApp(App()),
+  );
+  runZonedGuarded<Future<void>>(() async => runApp(App()), (error, stackTrace) async {
+    await Sentry.captureException(
+      error,
+      stackTrace: stackTrace,
+    );
+  });
+  // runApp(App());
+}
+
+/// A [StatelessWidget] which uses:
+/// * [bloc](https://pub.dev/packages/bloc)
+/// * [flutter_bloc](https://pub.dev/packages/flutter_bloc)
+/// to manage the state of a counter.
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeData>(
+        builder: (_, theme) {
+          return MaterialApp(
+            theme: theme,
+            home: BlocProvider(
+              create: (_) => CounterBloc(),
+              child: CounterPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+/// A [StatelessWidget] which demonstrates
+/// how to consume and interact with a [CounterBloc].
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (_, count) {
+          return Center(
+            child: Text('$count', style: Theme.of(context).textTheme.headline1),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () => context.read<CounterBloc>().add(CounterEvent.increment),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: const Icon(Icons.remove),
+              onPressed: () => context.read<CounterBloc>().add(CounterEvent.decrement),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: const Icon(Icons.brightness_6),
+              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.error),
+              onPressed: () => context.read<CounterBloc>().add(CounterEvent.error),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// Event being processed by [CounterBloc].
+enum CounterEvent {
+  /// Notifies bloc to increment state.
+  increment,
+
+  /// Notifies bloc to decrement state.
+  decrement,
+
+  /// Notifies the bloc of an error
+  error,
+}
+
+/// {@template counter_bloc}
+/// A simple [Bloc] which manages an `int` as its state.
+/// {@endtemplate}
+class CounterBloc extends Bloc<CounterEvent, int> {
+  /// {@macro counter_bloc}
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+      case CounterEvent.error:
+        addError(Exception('unsupported event'));
+    }
+  }
+}
+
+/// {@template brightness_cubit}
+/// A simple [Cubit] which manages the [ThemeData] as its state.
+/// {@endtemplate}
+class ThemeCubit extends Cubit<ThemeData> {
+  /// {@macro brightness_cubit}
+  ThemeCubit() : super(_lightTheme);
+
+  static final _lightTheme = ThemeData(
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.white,
+    ),
+    brightness: Brightness.light,
+  );
+
+  static final _darkTheme = ThemeData(
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.black,
+    ),
+    brightness: Brightness.dark,
+  );
+
+  /// Toggles the current brightness between light and dark.
+  void toggleTheme() {
+    emit(state.brightness == Brightness.dark ? _lightTheme : _darkTheme);
+  }
+}
